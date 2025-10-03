44e51286ecf8e7b220f3e7d9016de0d95d9bcd79
Bruno Silva <bruno.ms1silva@gmail.com>
2025-10-03 14:29:40 +0100

    Added libraries and nextUsers

--- bci-core/src/bci/Library.java
+++ bci-core/src/bci/Library.java
@@ -3,7 +3,9 @@ package bci;
 import bci.exceptions.*;
 import java.io.IOException;
 import java.io.Serializable;
-//FIXME maybe import classes
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
 
 /** Class that represents the library as a whole. */
 class Library implements Serializable {
@@ -11,6 +13,16 @@ class Library implements Serializable {
     @java.io.Serial
     private static final long serialVersionUID = 202507171003L;
 
+    private int currentDate = 0;
+
+    /* private Map<Integer, User> users = new HashMap<>();
+
+    private Map<Integer, Work> works = new HashMap<>();
+
+    private Map<String, Creator> creators = new HashMap<>(); */
+
+    private int nextUserId = 1;
+    private int nextWorkId = 1;
     //FIXME maybe define attributes
     //FIXME maybe implement constructor
     //FIXME maybe implement methods
